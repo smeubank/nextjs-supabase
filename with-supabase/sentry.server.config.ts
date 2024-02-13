c2fdb0187735a849028ccfae614ef6cfb151bf2c
Steven Eubank <47563310+smeubank@users.noreply.github.com>
2024-02-13 13:34:10 +0000

    add sentry and other stuff

---
+++ with-supabase/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://deec672a1bee42c04f6dfce3f3ebc13a@o673219.ingest.sentry.io/4506739726483456",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
